545f7e5559aab1d425a383305c03ebbec1fd0b41
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2024-09-07 23:14:11 +0600

    feat: Add notice in top and marquee animation
    - Updated the global CSS file to include a new keyframe animation called "marquee".
    - The animation is used to create a scrolling effect for text elements.
    - Added a hover state to pause the animation when the element is hovered over.

---
+++ src/components/common/Notice.jsx
@@ -0,0 +1,14 @@
+
+const Notice = () => {
+  return (
+    <div className="bg-green-300 py-2 overflow-hidden">
+      <div className="whitespace-nowrap animate-marquee">
+        <p className="text-center text-md font-normal text-gray-900 inline-block">
+          🎯 অনলাইনে ৮৫% স্কলারশিপে PROFESSIONAL GRAPHIC DESIGN COURSE | 🎯 অনলাইনে ৮৫% স্কলারশিপে PROFESSIONAL GRAPHIC DESIGN COURSE
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Notice;
