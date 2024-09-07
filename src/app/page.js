604b116d3665f6a1c3fc5706a678deaa70a880c7
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2024-09-07 13:36:19 +0600

    feat: Add favicon and hero image in Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import { ThemeProvider } from 'next-themes';
 import Footer from "@/components/common/Footer";
 import Header from "@/components/common/Header";
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <ThemeProvider attribute="class">
 		<Header/>
+		<div>
+			<Image src="/banner.jpg" alt="Hero Image" width={2000} height={300} className="w-full h-auto" />
+		</div>
 		<Footer/>
 	</ThemeProvider>
   );
