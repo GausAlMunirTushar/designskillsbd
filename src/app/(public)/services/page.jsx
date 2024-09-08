b396020759c99febec5c15a5b296f6ce695d899c
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2024-09-08 21:11:02 +0600

    feat: Add Services component to ServicesPage

--- src/app/(public)/services/page.jsx
+++ src/app/(public)/services/page.jsx
@@ -1,9 +1,10 @@
+import Services from '@/components/service/Services'
 import React from 'react'
 
 const ServicesPage = () => {
   return (
-	<div className="container mx-auto px-4">
-		Sericves
+	<div className="container mx-auto px-4 py-4">
+		<Services/>
 	</div>
   )
 }
